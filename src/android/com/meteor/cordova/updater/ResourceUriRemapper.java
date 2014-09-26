bb2df54eebd19dd30b02523ca42c30bbf4e624f0
Justin SB <justin@fathomdb.com>
2014-09-26 09:30:16 -0700

    Cache assets (and also dump them)

--- src/android/com/meteor/cordova/updater/ResourceUriRemapper.java
+++ src/android/com/meteor/cordova/updater/ResourceUriRemapper.java
@@ -1,8 +1,9 @@
 package com.meteor.cordova.updater;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import android.content.res.AssetManager;
 import android.net.Uri;
@@ -11,7 +12,7 @@ import android.util.Log;
 public class ResourceUriRemapper implements UriRemapper {
     private static final String TAG = "meteor.cordova.updater";
 
-    final String assetBase;
+    final Asset assetBase;
     final AssetManager assetManager;
 
     public ResourceUriRemapper(AssetManager assetManager, String assetBase) {
@@ -21,39 +22,109 @@ public class ResourceUriRemapper implements UriRemapper {
         }
 
         this.assetManager = assetManager;
-        this.assetBase = assetBase;
+        this.assetBase = new Asset(null, assetBase);
+
+        // For debug...
+        this.assetBase.dump();
+    }
+
+    // The asset API is really slow; cache lookups
+    class Asset {
+        final String name;
+        final String path;
+
+        private List<Asset> children;
+
+        public Asset(String name, String path) {
+            this.name = name;
+            this.path = path;
+            assert !path.endsWith("/");
+        }
+
+        public List<Asset> getChildren() {
+            if (this.children == null) {
+                List<Asset> children;
+                String[] assets = null;
+                try {
+                    assets = assetManager.list(path);
+                } catch (IOException e) {
+                    Log.w(TAG, "Error listing assets at " + path, e);
+                }
+                if (assets == null || assets.length == 0) {
+                    children = Collections.emptyList();
+                } else {
+                    children = new ArrayList<Asset>(assets.length);
+                    for (String asset : assets) {
+                        children.add(new Asset(asset, path + "/" + asset));
+                    }
+                }
+                this.children = children;
+            }
+            return this.children;
+        }
+
+        public void dump() {
+            Log.i(TAG, "Found asset: " + this.path);
+            for (Asset child : getChildren()) {
+                child.dump();
+            }
+        }
+
+        public boolean exists(String path) {
+            String[] pathTokens = path.split("/");
+            Asset current = this;
+            for (String pathToken : pathTokens) {
+                current = current.getChild(pathToken);
+                if (current == null) {
+                    return false;
+                }
+            }
+            return current != null;
+        }
+
+        private Asset getChild(String name) {
+            for (Asset child : getChildren()) {
+                if (child.name.equals(name)) {
+                    return child;
+                }
+            }
+            return null;
+        }
     }
 
     @Override
     public Uri remapUri(Uri uri) {
         String path = uri.getPath();
+
         assert path.startsWith("/");
-        assert !assetBase.endsWith("/");
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
 
-        String assetPath = assetBase + path;
-        Log.d(TAG, "Asset path is " + assetPath);
+        // String assetPath = assetBase.path + path;
+        // Log.d(TAG, "Asset path is " + assetBase.path + path);
 
-        if (assetExists(assetPath)) {
-            Log.d(TAG, "Remapping to " + "file:///android_asset/" + assetPath);
-            return Uri.parse("file:///android_asset/" + assetPath);
+        if (assetBase.exists(path)) {
+            Log.d(TAG, "Remapping to " + "file:///android_asset/" + assetBase.path + "/" + path);
+            return Uri.parse("file:///android_asset/" + assetBase.path + "/" + path);
         }
 
         return null;
     }
 
-    private boolean assetExists(String assetPath) {
-        InputStream is = null;
-        try {
-            is = assetManager.open(assetPath);
-        } catch (FileNotFoundException e) {
-            return false;
-        } catch (IOException e) {
-            Log.d(TAG, "Error while opening " + assetPath + "(" + e + ")");
-            return false;
-        } finally {
-            Utils.closeQuietly(is);
-        }
-        return true;
-    }
+    // private boolean assetExists(String assetPath) {
+    // InputStream is = null;
+    // try {
+    // is = assetManager.open(assetPath);
+    // } catch (FileNotFoundException e) {
+    // return false;
+    // } catch (IOException e) {
+    // Log.d(TAG, "Error while opening " + assetPath + "(" + e + ")");
+    // return false;
+    // } finally {
+    // Utils.closeQuietly(is);
+    // }
+    // return true;
+    // }
 
 }
